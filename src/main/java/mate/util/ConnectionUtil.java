7634e20c67fb917e8b4cf67d541aeedb45e83a31
Kostia Dementiev <konstde00@gmail.com>
2021-10-20 21:42:41 +0300

    prepared ConnectionUtil

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,12 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi_db?"
-            + "useUnicode=true&useJDBCCompliant"
-            + "TimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "1331";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "Your link to database";
+    private static final String USERNAME = "Your username";
+    private static final String PASSWORD = "Your password";
+    private static final String JDBC_DRIVER = "Your driver";
 
     static {
         try {
